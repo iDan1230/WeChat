b0eae6f05516ad872781020039afb5c0f9fe39c3
Administrator <zhidan_yang@163.com>
2018-05-18 17:48:54 +0800

    添加基础录音和播放功能

--- pages/detaile/detaile.js
+++ pages/detaile/detaile.js
@@ -5,8 +5,8 @@ Page({
    * 页面的初始数据
    */
   data: {
-    id:null
-    
+    id: null,
+    filePath:null
 
   },
 
@@ -15,64 +15,118 @@ Page({
    */
   onLoad: function (options) {
     wx.setNavigationBarTitle({
-      
+
       title: wx.getStorageSync('title')//接受上一个页面的参数
 
     })
 
     this.setData({
-      id:wx.getStorageSync('id')
+      id: wx.getStorageSync('id')
     })
 
     console.log(this.id)
+  },
+
+  start: function () {
+
+    const recorderManager = wx.getRecorderManager()
+
+    recorderManager.onStart(() => {
+      console.log('recorder start')
+    })
+    recorderManager.onPause(() => {
+      console.log('recorder pause')
+    })
+    recorderManager.onStop((res) => {
+      console.log('recorder stop', res)
+      const { tempFilePath } = res
+      this.filePath = tempFilePath;
+      wx.showToast({
+        title: tempFilePath,
+      })
+      console.log(this.filePath)
+    })
+    recorderManager.onFrameRecorded((res) => {
+      console.log('recorder22222222', res)
+      const { frameBuffer } = res
+      console.log('frameBuffer.byteLength', frameBuffer.byteLength)
+    })
+
+    const options = {
+      duration: 10000,
+      sampleRate: 44100,
+      numberOfChannels: 1,
+      encodeBitRate: 192000,
+      format: 'aac',
+      frameSize: 50
+    }
+
+    recorderManager.start(options)
+  },
+
+  play: function(){
+
+    var filePath = this.filePath;
+    console.log(filePath);
+    const innerAudioContext = wx.createInnerAudioContext()
+    innerAudioContext.autoplay = true
+    innerAudioContext.src = this.filePath
+    innerAudioContext.onPlay(() => {
+      console.log('开始播放')
+    })
+    innerAudioContext.onError((res) => {
+      console.log(res.errMsg)
+      console.log(res.errCode)
+    })
+
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+
   }
 })
